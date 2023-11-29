1bf91068d3223f843a36f9acb6262e0176f419bb
Roman <112933914+rump1ne@users.noreply.github.com>
2023-11-29 18:46:30 +0700

    Create strlen.cpp

---
+++ strlen.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+size_t* strlen(char* str) {
+    size_t result = 0;
+    for (size_t i = 0; str[i] i= '\n' || str[i] i= '\0'; i++)
+    result++
+    return result;
+    
+}
